ec81e4870ad7af1b7ae3dc1b26be8cad07418f8b
Suyeol Jeon <devxoul@gmail.com>
2021-08-22 19:57:04 +0900

    Integrate app

--- index.js
+++ index.js
@@ -1,21 +1,91 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Probot} app
- */
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+  app.on([
+    'pull_request.opened',
+    'pull_request.edited',
+    'pull_request.synchronize',
+  ], (context) => {
+    const octokit = context.octokit
+    const logger = app.log
+    logger.info(`Got an event: ${context.name}.`)
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
+    const repository = {
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+    }
+    const pullRequest = context.payload.pull_request
+    const isMergeable = getMergeable({ logger, pullRequest })
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    updateStatus({ octokit, logger, repository, pullRequest, isMergeable })
+    updateLabel({ octokit, logger, repository, pullRequest, isMergeable })
+  })
+}
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-};
+const getMergeable = ({ logger, pullRequest }) => {
+  const targetBranch = pullRequest.base.ref
+  logger.info(`Target branch is ${targetBranch}.`)
+
+  const isMergeable = (
+    targetBranch === 'main' ||
+    targetBranch.startsWith('release/') ||
+    targetBranch.startsWith('hotfix/')
+  )
+  logger.info(`This pull request is ${isMergeable ? '' : 'not '}mergeable.`)
+
+  return isMergeable
+}
+
+const updateStatus = ({ octokit, logger, repository, pullRequest, isMergeable }) => {
+  logger.info(`Updating status.`)
+  octokit
+    .request('POST /repos/:owner/:repo/statuses/:sha', {
+      ...repository,
+      sha: pullRequest.head.sha,
+      target_url: `https://github.com/${repository.owner}/${repository.repo}`,
+      context: 'Can I merge this PR?',
+      description: isMergeable ? 'Let\'s go!' : 'Wait...',
+      state: isMergeable ? 'success' : 'pending',
+    })
+    .then(() => logger.info(`Updated status.`))
+    .catch(error => logger.error(`Failed to update status. ${error}`))
+}
+
+const updateLabel = ({ isMergeable, ...params }) => {
+  const label = 'status: dependent'
+  if (!isMergeable) {
+    addLabel({ ...params, label })
+  } else {
+    removeLabel({ ...params, label })
+  }
+}
+
+const addLabel = ({ octokit, logger, repository, pullRequest, label }) => {
+  logger.info(`Adding label.`)
+
+  const exists = pullRequest.labels.find(x => x.name === label)
+  if (exists) {
+    logger.info('Label already exists. Skip.')
+    return
+  }
+
+  octokit.issues
+    .addLabels({
+      ...repository,
+      issue_number: pullRequest.number,
+      labels: [label],
+    })
+    .then(() => logger.info(`Added label.`))
+    .catch(error => logger.error(`Failed to add label. ${error}`))
+}
+
+const removeLabel = ({ octokit, logger, repository, pullRequest, label }) => {
+  logger.info(`Removing label.`)
+
+  octokit.issues
+    .removeLabel({
+      ...repository,
+      issue_number: pullRequest.number,
+      name: label,
+    })
+    .then(() => logger.info(`Removed label.`))
+    .catch(error => logger.error(`Failed to remove label. ${error}`))
+}
